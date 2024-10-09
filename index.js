63bc2085ba29d642546860ed9f842dd31075bb1d
friress <mickaelmaldonado2@gmail.com>
2024-10-09 15:21:58 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,15 +1,35 @@
-const Discord = require("discord.js")
-const bot = new Discord.Client({intents: 53608447})
-const config = require("./config.js")
+const Discord = require("discord.js");
+const bot = new Discord.Client({ intents: 53608447 });
+const config = require("./config.js");
+const { EmbedBuilder } = require("@discordjs/builders");
 
-bot.login(config.token)
+bot.login(config.token);
 
 bot.once("ready", () => {
-    console.log(`${bot.user.tag} est bien en ligne`)
-})
+  console.log(`${bot.user.tag} est bien en ligne`);
+});
 
 bot.on("messageCreate", (message) => {
-    if (message.content === "!ping") {
-        message.channel.send("Pong!");
+  if (message.content === "!ping") {
+    message.channel.send("Pong!");
+  }
+
+  if (message.content.startsWith("!avatar")) {
+    const username = message.content.split(" ")[1];
+    const user = message.guild.members.cache.find(
+      (member) => member.user.username === username,
+    )?.user;
+
+    if (user) {
+      const embed = new EmbedBuilder()
+        .setTitle(`${user.username}`)
+        .setImage(user.displayAvatarURL({ dynamic: true, size: 1024 }))
+        .setColor(0x5b209a)
+        .setDescription(`Voici l'avatar de ${user.username}`)
+
+      message.channel.send({ embeds: [embed] });
+    } else {
+      message.channel.send("Erreur");
     }
-})
+  }
+});
