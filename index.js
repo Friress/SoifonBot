e20eb5a0a43aec0e9edf0a1395ad13c3a953bb8b
friress <mickaelmaldonado2@gmail.com>
2024-10-11 14:37:07 +0200

    Update index.js

--- index.js
+++ index.js
@@ -25,11 +25,47 @@ bot.on("messageCreate", (message) => {
         .setTitle(`${user.username}`)
         .setImage(user.displayAvatarURL({ dynamic: true, size: 1024 }))
         .setColor(0x5b209a)
-        .setDescription(`Voici l'avatar de ${user.username}`)
+        .setDescription(`Voici l'avatar de ${user.username}`);
 
       message.channel.send({ embeds: [embed] });
     } else {
       message.channel.send("Erreur");
+    }
+  }
+});
+
+bot.on("messageCreate", async (message) => {
+  if (!message.content.startsWith("!")) return;
+
+  const args = message.content.slice(1).trim().split(/ +/);
+  const command = args.shift().toLowerCase();
+
+  if (command === "ban" || command === "kick") {
+    // Check if the user has the required permissions
+    if (!message.member.permissions.has(["BAN_MEMBERS", "KICK_MEMBERS"])) {
+      return message.reply(
+        "Vous n'avez pas les permissions pour utiliser cette comande.",
+      );
+    }
+
+    // Check if a user is mentioned
+    const user = message.mentions.users.first();
+    if (!user) {
+      return message.reply("Veuillez mentionner un utilisateur.");
+    }
+
+    try {
+      // Ban or kick the mentioned user
+      if (command === "ban") {
+        await message.guild.members.ban(user.id);
+        message.reply(`${user.tag} a été banni.`);
+      } else if (command === "kick") {
+        await message.guild.members.kick(user.id);
+        message.reply(`${user.tag} a été kick.`);
+      }
+    } catch (error) {
+      console.error(error);
+      message.reply(`Il y'a eu un problème dans la commande.`);
     }
   }
 });
